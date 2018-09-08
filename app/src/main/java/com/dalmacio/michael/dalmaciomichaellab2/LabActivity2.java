1e79913b228a63c0c1a99a96b728ba5979736eac
Michael-Antonio-Dalmacio <2015081255@ust-ics.mygbiz.com>
2018-09-08 18:25:54 +0800

    Initial commit

---
+++ app/src/main/java/com/dalmacio/michael/dalmaciomichaellab2/LabActivity2.java
@@ -0,0 +1,13 @@
+package com.dalmacio.michael.dalmaciomichaellab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LabActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayoutdalmacio);
+    }
+}
